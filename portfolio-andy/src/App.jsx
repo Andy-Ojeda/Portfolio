1c6fd5e6e6449fe8c3e8100aa436695a1ff99a1c
Andy Ojeda <tuar_a@hotmail.com>
2023-11-29 10:05:26 -0300

    Cambios en edición del edificio

--- portfolio-andy/src/App.jsx
+++ portfolio-andy/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 import './App.css'
 import imageFondo from './assets/fondo.png';
 import imageEdificio from './assets/edificio22.png';
 import imagePersonaje from './assets/personaje.png';
 import imageTitulo from './assets/tituloAndyO22.png';
+import personajeEdificio from './assets/personajeEdificio.png';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <div className="contAll">
@@ -20,19 +20,22 @@ function App() {
             <img src={imageTitulo} alt="" />
           </div>
           <div className='contEdifPerso'>
-              <div className='contPersonaje'>
+              <img src={personajeEdificio} alt='edificio' />
+
+              {/* <div className='contPersonaje'>
                 <img src={imagePersonaje} alt="" />
               </div>
               
               <div className='contEdificio'>
                 <img src={imageEdificio} alt="" />
-              </div>
+              </div> */}
           </div>
 
       </div>
 
 
     </>
-  )}
+  )
+}
 
 export default App;
